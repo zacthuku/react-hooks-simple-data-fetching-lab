320cee208931b9cd9474d23e0fd91d58b691c895
Zacharia Ndinguri <zacthuku@gmail.com>
2025-04-16 10:11:52 +0300

    ddata fetching lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,26 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [dogImage, setDogImage] = useState(null);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((res) => res.json())
+      .then((data) => {
+        setDogImage(data.message);
+      });
+  }, []);
+
+  if (!dogImage) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div>
+      <img src={dogImage} alt="A Random Dog" />
+    </div>
+  );
+}
+
+export default App;
